9134f610895eca8a1dd4bea5bcfbcf979e2e9349
thiswillbeyourgithub <26625900+thiswillbeyourgithub@users.noreply.github.com>
2024-11-19 17:48:40 +0100

    go back to 4bit
    Signed-off-by: thiswillbeyourgithub <26625900+thiswillbeyourgithub@users.noreply.github.com>

--- research/research.py
+++ research/research.py
@@ -54,14 +54,14 @@ model_name = "mistralai/Mistral-7B-Instruct-v0.3"
 
 print(f"Selected model: {model_name}")
 bnb_config = BitsAndBytesConfig(
-  load_in_8bit=True,
-  bnb_8bit_quant_type="nf8",
-  bnb_8bit_compute_dtype=torch.bfloat16,
-  bnb_8bit_use_double_quant=True,
-  # load_in_4bit=True,
-  # bnb_4bit_quant_type="nf4",
-  # bnb_4bit_compute_dtype=torch.bfloat16,
-  # bnb_4bit_use_double_quant=True,
+  # load_in_8bit=True,
+  # bnb_8bit_quant_type="nf8",
+  # bnb_8bit_compute_dtype=torch.bfloat16,
+  # bnb_8bit_use_double_quant=True,
+  load_in_4bit=True,
+  bnb_4bit_quant_type="nf4",
+  bnb_4bit_compute_dtype=torch.bfloat16,
+  bnb_4bit_use_double_quant=True,
 )
 
 print("Initializing tokenizer...")
