ae749a095b16265fab3c69ad639d199b28c1776f
thiswillbeyourgithub <26625900+thiswillbeyourgithub@users.noreply.github.com>
2024-11-20 20:52:08 +0100

    typo
    Signed-off-by: thiswillbeyourgithub <26625900+thiswillbeyourgithub@users.noreply.github.com>

--- research/research.py
+++ research/research.py
@@ -69,7 +69,7 @@ print("Initializing tokenizer...")
 tokenizer = AutoTokenizer.from_pretrained(
     model_name,
     gguf_file=fname,
-    device_map="auto",  # may oom on low vram, otherwise use all available gous I think
+    device_map="auto",  # may oom on low vram, otherwise use all available gpus I think
     # low_cpu_mem_usage=True,  # avoids oom when loading the model but takes much more time to load the model
     quantization_config=bnb_config,
 )
@@ -81,7 +81,7 @@ model = AutoModelForCausalLM.from_pretrained(
     model_name,
     gguf_file=fname,
     device_map="auto",
-    # device_map="cuda",  # may oom on low vram, otherwise use all available gous I think
+    # device_map="cuda",  # may oom on low vram, otherwise use all available gpus I think
     # low_cpu_mem_usage=True,  # avoids oom when loading the model but takes much more time to load the model
     quantization_config=bnb_config,
 )
