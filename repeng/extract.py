5a9f4ec65f543a3b984b374f1a9f0304e376142b
thiswillbeyourgithub <26625900+thiswillbeyourgithub@users.noreply.github.com>
2024-11-20 18:57:35 +0100

    disable arg low_memory for umap
    Signed-off-by: thiswillbeyourgithub <26625900+thiswillbeyourgithub@users.noreply.github.com>

--- repeng/extract.py
+++ repeng/extract.py
@@ -310,7 +310,7 @@ def read_representations(
 
             umap_model = umap.UMAP(
                 n_components=1,
-                low_memory=True,
+                # low_memory=True,
                 random_state=42,
                 transform_seed=42,
                 # densmap=True,
