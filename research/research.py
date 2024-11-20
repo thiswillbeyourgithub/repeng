bb5746cabeeae412902d8bdea3fc7707599bf9a2
thiswillbeyourgithub <26625900+thiswillbeyourgithub@users.noreply.github.com>
2024-11-20 21:21:16 +0100

    pca
    Signed-off-by: thiswillbeyourgithub <26625900+thiswillbeyourgithub@users.noreply.github.com>

--- research/research.py
+++ research/research.py
@@ -139,8 +139,8 @@ trippy_vector = ControlVector.train(
     tokenizer,
     trippy_dataset,
     batch_size=1,
-    # method="pca_diff",
-    method="pca_center",  # seems more stable, whereas pca_diff quickly seems to be unreadable
+    method="pca_diff",
+    # method="pca_center",  # seems more stable, whereas pca_diff quickly seems to be unreadable
     # method="umap",  # garbage
     # method="pacmap",
 )
