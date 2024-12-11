c0595d870c8d463fb1f27538fc955b972a27f0cb
thiswillbeyourgithub <26625900+thiswillbeyourgithub@users.noreply.github.com>
2024-12-11 14:18:08 +0100

    minor
    Signed-off-by: thiswillbeyourgithub <26625900+thiswillbeyourgithub@users.noreply.github.com>

--- research/research.py
+++ research/research.py
@@ -180,7 +180,10 @@ scenario = autocorrect_chat_templates(
 
 # set the control strength and let inference rip!
 print("Applying strength vectors")
-strengths = [-3, -2, -1] +  [r/10 for r in range(-5, 6, 1)] + [1, 2, 3]
+strengths = [-5, -3, -2, -1]
+# strengths += [0]
+strengths += [r/10 for r in range(-5, 6, 1)]
+strengths += [1, 2, 3, 5]
 for strength in strengths:
     print("#" * 20 + f" Strength={strength}")
     model.set_control(perturb_vector, strength)
