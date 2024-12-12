34f93b3d247d4c40f3e10014ea381dbaeeb82d4f
thiswillbeyourgithub <26625900+thiswillbeyourgithub@users.noreply.github.com>
2024-12-12 17:06:20 +0100

    add veartype
    Signed-off-by: thiswillbeyourgithub <26625900+thiswillbeyourgithub@users.noreply.github.com>

--- repeng/__init__.py
+++ repeng/__init__.py
@@ -4,6 +4,13 @@ import numpy as np
 import torch
 from transformers import PreTrainedModel, PreTrainedTokenizerBase
 
+from beartype import BeartypeConf
+from beartype.claw import beartype_all, beartype_this_package
+# beartype_this_package()
+beartype_this_package(conf=BeartypeConf(violation_type=UserWarning))
+# beartype_all(conf=BeartypeConf(violation_type=UserWarning))
+
+
 from . import control, extract
 from .extract import ControlVector
 from .control import ControlModel
