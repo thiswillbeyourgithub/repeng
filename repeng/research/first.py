c15dae591014c71d1a863a1f79629b926407663b
thiswillbeyourgithub <26625900+thiswillbeyourgithub@users.noreply.github.com>
2025-09-05 15:35:58 +0200

    fix question
    Signed-off-by: thiswillbeyourgithub <26625900+thiswillbeyourgithub@users.noreply.github.com>

--- repeng/research/first.py
+++ repeng/research/first.py
@@ -73,7 +73,10 @@ trained_vector = ControlVector.train(
 # Now we must give the scenario for the generation we will engineer:
 # By turning chat messages into the format expected by the model [RECOMMENDED]
 scenario: str = tokenizer.apply_chat_template(
-    "My IQ got tested at precisely",
+    conversation={
+        "role": "user",
+        "content": "My IQ got tested at precisely",
+    },
     continue_final_message=True,
     tokenize=False,
 )
